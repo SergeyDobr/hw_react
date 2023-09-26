bf271d379d4a129b580999be0e121f10cf48d7d9
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2023-09-26 17:33:24 +0300

    new init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const User = (pro) => {
+  return (
+    <div>
+      <p>Name: {pro.name}</p>
+      <p>Age: {pro.age}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <User name="Serhii" age="25" />
+      <User name="Alina" age="21" />
+    </div>  
+  )
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
