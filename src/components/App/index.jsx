f18d147a0ba9c73b108fbc28c836bb25e3b73aea
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2023-10-06 17:27:08 +0300

    default

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,6 @@
-import Slider from '../Slider'; // Предполагая, что ваш компонент Slider находится в файле Slider.js
-
 const App = () => {
    return (
       <div className="App">
-         <Slider />
       </div>
    );
 }
